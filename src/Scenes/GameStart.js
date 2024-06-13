10f9d1f9cf9cd71429ff7a268c31b49e66c644d6
PeichenY <pyao3@ucsc.edu>
2024-06-12 19:08:44 -0700

    some change
    Try to make the game title look a bit cleaner

--- src/Scenes/GameStart.js
+++ src/Scenes/GameStart.js
@@ -24,14 +24,10 @@ class GameStart extends Phaser.Scene{
         this.add.image(100,300,'aKey').setScale(3);
         this.add.image(150,300,'sKey').setScale(3);
         this.add.image(200,300,'dKey').setScale(3);
-        this.add.image(110,450,'space1').setScale(3);
-        this.add.image(150,450,'space2').setScale(3);
-        this.add.image(190,450,'space3').setScale(3);
         this.add.image(430,300,'boss1').setScale(1.5);
         
         this.add.bitmapText(150,200,"kenneyFont", "Controls", 32).setOrigin(0.5);
         this.add.bitmapText(150,350,"kenneyFont", "to move", 32).setOrigin(0.5);
-        this.add.bitmapText(150,500,"kenneyFont", "to shoot", 32).setOrigin(0.5);
         this.add.bitmapText(480,200,"kenneyFont", "Finish the Boss", 32).setOrigin(0.5);
         this.add.bitmapText(320,650,"kenneyFont", "Press SPACE to start", 32).setOrigin(0.5);
         this.input.keyboard.on('keydown-SPACE', ()=> {
